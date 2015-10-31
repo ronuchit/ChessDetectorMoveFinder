3929a6509f3064eb4daf7932793577fc315f8b8e
Harrison Wang <h.wang94@berkeley.edu>
2015-10-30 23:49:10 -0700

    before pull unrotate

--- src/detector.py
+++ src/detector.py
@@ -5,6 +5,8 @@ import cv2
 from matplotlib import pyplot as plt
 from scipy import misc
 import pdb
+import unrotate
+
 
 SECONDS_TO_WAIT = 3
 plt.rcParams['image.cmap'] = 'gray' # set default image to grayscale
@@ -13,7 +15,7 @@ IMAGE_FOLDER = "../images/samples/"
 
 img_filename = IMAGE_FOLDER + "pic1_cropped.png"
 SLEEP = False
-L_THRESH = 50
+L_THRESH = 80
 H_THRESH = 200
 
 def get_counts(edge_img, i, max_h, window_size):
@@ -32,8 +34,23 @@ def get_rows(edges,window_size=2):
     for i in xrange(0, h):
         count = get_counts(edges, i, h, window_size)
         row_counts.append(count)
-    pdb.set_trace()
     return row_counts
+
+def get_sorted_indexes(counts):
+    indx = sorted(range(len(counts)), key = lambda k: counts[k])
+    indx.reverse()
+    return indx
+
+def get_hough_transform_lines(image, edges):
+    minLineLength = 300
+    maxLineGap = 10
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, minLineLength, maxLineGap)
+    for (x1, y1, x2, y2) in lines[0]:
+        cv2.line(image, (x1, y1), (x2, y2), (0,255,0), 2)
+    return (image, edges)
+    #plt.imshow(image)
+    #plt.show()
+
 
 # given images
 while(True):
@@ -44,20 +61,56 @@ while(True):
     gray = np.float32(gray)
     edges = cv2.Canny(image, L_THRESH, H_THRESH)
 
-    dst = cv2.cornerHarris(gray, 2, 5, 0.04)
-    dst = cv2.dilate(dst, None)
-    image[dst>0.01*dst.max()] = [0,0,255]
+    #image, edges = get_hough_transform_lines(image, edges)
+
     # count rows and cols where edges are within a certain window_size range
-    #row = get_rows(edges)
-    #rotated = np.rot90(edges)
-    #cols = get_rows(rotated)
-    #plt.imshow(image)
-    #plt.show()
-    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    pdb.set_trace()
-    test = cv2.drawContours(image, contours, -1, (0, 255, 0), 3)
-    cv2.imshow('image', image)
-    cv2.waitKey(0)
+    h, w = edges.shape
+    row_counts = get_rows(edges)
+    max_row_indexes = get_sorted_indexes(row_counts)
+    curr_index = max_row_indexes[0]
+    edges[curr_index, :] = 255
+    rows_changed = [curr_index]
+    num_rows_changed = 1
+    for idx, row_idx in enumerate(max_row_indexes):
+        #actual_row = row_counts[row_idx]
+        in_range = False
+        for r_changed in rows_changed:
+            if (row_idx < r_changed+25 and row_idx > r_changed-25):
+                in_range = True
+        if (not in_range):
+            edges[row_idx, :] = 255
+            rows_changed.append(row_idx)
+            num_rows_changed += 1
+            if (num_rows_changed > 10):
+                break
+
+    #max_row_index = np.argmax(row_counts)
+    rotated = np.rot90(edges)
+    col_counts = get_rows(rotated)
+    max_row_indexes = get_sorted_indexes(col_counts)
+    curr_index = max_row_indexes[0]
+    edges[:, w-curr_index] = 255
+    rows_changed = [curr_index]
+    num_rows_changed = 1
+    for idx, row_idx in enumerate(max_row_indexes):
+        #actual_row = row_counts[row_idx]
+        in_range = False
+        for r_changed in rows_changed:
+            if (row_idx < r_changed+25 and row_idx > r_changed-25):
+                in_range = True
+        if (not in_range):
+            edges[:, w-row_idx] = 255
+            rows_changed.append(row_idx)
+            num_rows_changed += 1
+            if (num_rows_changed > 10):
+                break
+
+    #max_col_index = np.argmax(col_counts)
+    h, w = edges.shape
+    #edges[max_row_index, :] = 255
+    #edges[:, w-max_col_index] = 255
+    plt.imshow(edges)
+    plt.show()
 
     pdb.set_trace()
     if SLEEP: # just cause i dont want it to sleep each time i test right now
